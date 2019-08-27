e8e7d6cc23d6d915c6e94a789f62ea6fcc44cd41
Yee Hing Tong <ytong@lyft.com>
2019-08-27 08:20:32 -0700

    remove more things, add a test

--- flytetester/tests/unit/sample_tasks_test.py
+++ flytetester/tests/unit/sample_tasks_test.py
@@ -1,19 +1,16 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-from app.workflows.sample_tasks import identity_sub_task, simple_batch_task, add_one_and_print
-
-
-def test_identity_sub_task():
-    outputs = identity_sub_task.unit_test(num=5)
-    assert outputs['outs'] == 5
-
-
-def test_simple_batch_task():
-    outputs = simple_batch_task.unit_test(iterations=10)
-    assert outputs['out'] == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+from app.workflows.sample_tasks import add_one_and_print
+from app.workflows.work import find_odd_numbers_with_string
 
 
 def test_add_one_and_print():
     outs = add_one_and_print.unit_test(value_to_print=15)
     assert outs['out'] == 16
+
+
+def test_find_odd_numbers_with_string():
+    outs = find_odd_numbers_with_string.unit_test(list_of_nums=[3,4], demo_string='hello world')
+    assert outs['altered_string'] == 'hello world_changed'
+    assert outs['are_num_odd'] == [True, False]
