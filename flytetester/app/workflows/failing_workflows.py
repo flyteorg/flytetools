b7aedda59bc60d8b16ba99630fa243c4cbab74c0
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-06-15 15:14:31 -0700

    Additional tests (#14)
    - [X] Retryable Dynamic Nodes
    - [X] RunToCompletion Workflow

--- flytetester/app/workflows/failing_workflows.py
+++ flytetester/app/workflows/failing_workflows.py
@@ -4,13 +4,19 @@ from __future__ import print_function
 import time
 from datetime import timedelta
 
-from flytekit.sdk.tasks import outputs, python_task
+from flytekit.sdk.tasks import (
+    python_task,
+    dynamic_task,
+    inputs,
+    outputs,
+)
 from flytekit.sdk.types import Types
 from flytekit.sdk.workflow import workflow_class
 from flytekit.common.exceptions.base import FlyteRecoverableException
+from flytekit.models.core.workflow import WorkflowMetadata
 
 @outputs(answer=Types.Integer)
-@python_task
+@python_task(cpu_request="40m")
 def divider(wf_params, answer):
     answer.set(1 / 0)
 
@@ -21,7 +27,7 @@ class DivideByZeroWf(object):
 
 
 @outputs(answer=Types.Integer)
-@python_task(timeout=timedelta(1))
+@python_task(timeout=timedelta(1), cpu_request="10m")
 def oversleeper(wf_params, answer):
     time.sleep(10)
     answer.set(1)
@@ -32,7 +38,7 @@ class SleeperWf(object):
     zzz = oversleeper()
 
 
-@python_task(retries=2)
+@python_task(retries=2, cpu_request="20m")
 def retryer(wf_params):
     raise FlyteRecoverableException('This task is supposed to fail')
 
@@ -40,3 +46,36 @@ def retryer(wf_params):
 @workflow_class
 class RetrysWf(object):
     retried_task = retryer()
+
+
+@dynamic_task(retries=2, cpu_request="40m")
+def retryable_dynamic_node(wf_params):
+    yield divider()
+
+
+@inputs(in_str=Types.String)
+@python_task()
+def echo(wf_params, in_str):
+    wf_params.logging.warn(in_str)
+
+
+@workflow_class
+class FailingDynamicNodeWF(object):
+    should_fail_and_retry = retryable_dynamic_node()
+
+
+@workflow_class(on_failure=WorkflowMetadata.OnFailurePolicy.FAIL_AFTER_EXECUTABLE_NODES_COMPLETE)
+class RunToCompletionWF(object):
+    div_by_zero = divider()
+    echo_n = echo(in_str="should never run")
+
+    echo_n_2 = echo(in_str="should run first")
+    echo_n_2_2 = echo(in_str="should run next")
+    div_by_zero_2 = divider()
+
+    should_not_run = divider()
+
+    echo_n_2 >> echo_n_2_2 >> div_by_zero_2
+    div_by_zero >> echo_n
+    echo_n_2 >> should_not_run
+    div_by_zero >> should_not_run
