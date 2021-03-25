b86b2ffb3cd474d5e2bf71e479d9134903095d93
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2021-03-25 16:55:49 -0700

    Use flytectl (#30)

--- flytetester/end2end/run.sh
+++ flytetester/end2end/run.sh
@@ -12,11 +12,24 @@ flytekit_venv flyte-cli -h flyteadmin:81 --insecure register-project -n flytetes
 flytekit_venv pyflyte -c end2end/end2end.config register -p flytetester -d development workflows
 
 # Kick off workflows
+# This one needs an input, which is easier to do programmatically using flytekit
 flytekit_venv pyflyte -c end2end/end2end.config lp -p flytetester -d development execute app.workflows.work.WorkflowWithIO --b hello_world
-flytekit_venv pyflyte -c end2end/end2end.config lp -p flytetester -d development execute app.workflows.failing_workflows.DivideByZeroWf
-flytekit_venv pyflyte -c end2end/end2end.config lp -p flytetester -d development execute app.workflows.failing_workflows.RetrysWf
-flytekit_venv pyflyte -c end2end/end2end.config lp -p flytetester -d development execute app.workflows.failing_workflows.FailingDynamicNodeWF
-flytekit_venv pyflyte -c end2end/end2end.config lp -p flytetester -d development execute app.workflows.failing_workflows.RunToCompletionWF
+
+# Quick shortcut to get at the version
+arrIN=(${FLYTE_INTERNAL_IMAGE//:/ })
+VERSION=${arrIN[1]}
+
+flytectl --config /opt/go/config.yaml get launchplan -p flytetester -d development app.workflows.failing_workflows.DivideByZeroWf --version ${VERSION} --execFile DivideByZeroWf.yaml
+flytectl --config /opt/go/config.yaml create execution --execFile DivideByZeroWf.yaml -p flytetester -d development
+
+flytectl --config /opt/go/config.yaml get launchplan -p flytetester -d development app.workflows.failing_workflows.RetrysWf --version ${VERSION} --execFile Retrys.yaml
+flytectl --config /opt/go/config.yaml create execution --execFile Retrys.yaml -p flytetester -d development
+
+flytectl --config /opt/go/config.yaml get launchplan -p flytetester -d development app.workflows.failing_workflows.FailingDynamicNodeWF --version ${VERSION} --execFile FailingDynamicNodeWF.yaml
+flytectl --config /opt/go/config.yaml create execution --execFile FailingDynamicNodeWF.yaml -p flytetester -d development
+
+flytectl --config /opt/go/config.yaml get launchplan -p flytetester -d development app.workflows.failing_workflows.RunToCompletionWF --version ${VERSION} --execFile RunToCompletionWF.yaml
+flytectl --config /opt/go/config.yaml create execution --execFile RunToCompletionWF.yaml -p flytetester -d development
 
 # Make sure workflow does everything correctly
 flytekit_venv python end2end/validator.py
