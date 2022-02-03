7849dc3206f1292fedd29725f2e9ee7c85b8871e
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2022-02-03 13:38:31 -0800

    Run other tests
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- flytetester/functional-tests/run.sh
+++ flytetester/functional-tests/run.sh
@@ -7,7 +7,4 @@ set -ex
 # LATEST_VERSION=$(curl --silent "https://api.github.com/repos/flyteorg/flytesnacks/releases/latest" | jq -r .tag_name)
 LATEST_VERSION=v0.3.27
 
-# We register the workflows here to avoid a race condition between registering the latest version of flytesnacks and the execution of the functional tests.
-# flytectl register examples -p flytesnacks -d development --version $LATEST_VERSION
-
 flytekit_venv python functional-tests/run-tests.py $LATEST_VERSION P0,P1 functional-tests/functional-test.config core
