fed09b81cfdb66290b0d8493055100de3a7b6647
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2022-02-03 12:41:43 -0800

    Hardcode flytesnacks version
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- flytetester/functional-tests/run.sh
+++ flytetester/functional-tests/run.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-LATEST_VERSION=$(curl --silent "https://api.github.com/repos/flyteorg/flytesnacks/releases/latest" | jq -r .tag_name)
 
 # TODO: Leave this here while we don't have a new flytesnacks release, let's use a release that is not affected by the conditional bug.
+# LATEST_VERSION=$(curl --silent "https://api.github.com/repos/flyteorg/flytesnacks/releases/latest" | jq -r .tag_name)
 LATEST_VERSION=v0.3.27
 
 # We register the workflows here to avoid a race condition between registering the latest version of flytesnacks and the execution of the functional tests.
