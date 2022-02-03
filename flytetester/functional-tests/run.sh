49b5aa6765aad7a0a21450433f66b805ec47aeb2
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2022-02-03 14:37:01 -0800

    Use flytesnacks v0.1.1
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- flytetester/functional-tests/run.sh
+++ flytetester/functional-tests/run.sh
@@ -5,6 +5,6 @@ set -ex
 
 # TODO: Leave this here while we don't have a new flytesnacks release, let's use a release that is not affected by the conditional bug.
 # LATEST_VERSION=$(curl --silent "https://api.github.com/repos/flyteorg/flytesnacks/releases/latest" | jq -r .tag_name)
-LATEST_VERSION=v0.3.27
+LATEST_VERSION=v0.1.1
 
 flytekit_venv python functional-tests/run-tests.py $LATEST_VERSION P0,P1 functional-tests/functional-test.config core
