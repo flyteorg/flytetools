0ee486f56d2719c47332219a51114a3706cc29fb
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2022-02-03 16:02:07 -0800

    Remove hardcoded version of flytesnacks
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- flytetester/functional-tests/run.sh
+++ flytetester/functional-tests/run.sh
@@ -2,9 +2,6 @@
 
 set -ex
 
-
-# TODO: Leave this here while we don't have a new flytesnacks release, let's use a release that is not affected by the conditional bug.
-# LATEST_VERSION=$(curl --silent "https://api.github.com/repos/flyteorg/flytesnacks/releases/latest" | jq -r .tag_name)
-LATEST_VERSION=v0.1.1
+LATEST_VERSION=$(curl --silent "https://api.github.com/repos/flyteorg/flytesnacks/releases/latest" | jq -r .tag_name)
 
 flytekit_venv python functional-tests/run-tests.py $LATEST_VERSION P0,P1 functional-tests/functional-test.config core
