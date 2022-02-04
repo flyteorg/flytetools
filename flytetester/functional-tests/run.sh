4a0f849a10ed4b2c1227479b61ee9766fcd38cca
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2022-02-03 17:55:10 -0800

    Register flytesnacks examples from inside the pod
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- flytetester/functional-tests/run.sh
+++ flytetester/functional-tests/run.sh
@@ -4,4 +4,6 @@ set -ex
 
 LATEST_VERSION=$(curl --silent "https://api.github.com/repos/flyteorg/flytesnacks/releases/latest" | jq -r .tag_name)
 
+flytectl register examples -p flytesnacks -d development --config /opt/go/config.yaml
+
 flytekit_venv python functional-tests/run-tests.py $LATEST_VERSION P0,P1 functional-tests/functional-test.config core
