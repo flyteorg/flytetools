158bf53f938e0ea4a8f49d8effac9727f9d266db
Haytham Abuelfutuh <haytham@afutuh.com>
2022-04-12 00:55:54 -0700

    Move is_complete to is_done (#60)
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- flytetester/functional-tests/run-tests.py
+++ flytetester/functional-tests/run-tests.py
@@ -73,7 +73,7 @@ def schedule_workflow_group(tag: str, workflow_group: str, remote: FlyteRemote) 
     # Wait for all launch plans to finish
     attempt = 0
     while attempt == 0 or (
-        not all([lp.is_complete for lp in launch_plans]) and attempt < MAX_ATTEMPTS
+        not all([lp.is_done for lp in launch_plans]) and attempt < MAX_ATTEMPTS
     ):
         attempt += 1
         print(
